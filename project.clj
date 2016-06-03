3186397876b5373b098d714173a4cbf0c0bbc49f
Duncan McGreggor <oubiwann@gmail.com>
2016-06-03 10:09:15 -0500

    Added dev profile and dev-resources.

--- project.clj
+++ project.clj
@@ -9,4 +9,10 @@
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [org.rasdaman/rasj "9.0.4"]
                  [dire "0.5.3"]
-                 [org.clojure/tools.namespace "0.2.11"]])
+                 [org.clojure/tools.namespace "0.2.11"]]
+  :profiles {
+    :dev {
+      :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                     [slamhound "1.5.5"]]
+      :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+      :source-paths ["dev-resources/src"]}})
