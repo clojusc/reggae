918909ff9ddf0e012858192a06887801f2a21e2d
Duncan McGreggor <oubiwann@gmail.com>
2016-06-03 10:43:31 -0500

    Updated deps and group id.

--- project.clj
+++ project.clj
@@ -1,18 +1,20 @@
-(defproject reggae "0.1.0-SNAPSHOT"
+(defproject clojusc/reggae "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {:init-ns reggae.dev}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
                  [org.rasdaman/rasj "9.0.4"]
-                 [dire "0.5.3"]
-                 [org.clojure/tools.namespace "0.2.11"]]
+                 [dire "0.5.4"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [clojusc/twig "0.2.1"]]
   :profiles {
     :dev {
       :dependencies [[org.clojure/tools.namespace "0.2.11"]
                      [slamhound "1.5.5"]]
+      :plugins [[lein-kibit "0.1.2"]
+                [jonase/eastwood "0.2.3"]]
       :aliases {"slamhound" ["run" "-m" "slam.hound"]}
       :source-paths ["dev-resources/src"]}})
