5d412053f724afbea9c8f3fa69f097be5fb4e2fb
Duncan McGreggor <oubiwann@gmail.com>
2016-06-03 14:37:24 -0500

    Bumped version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/reggae "0.1.0-SNAPSHOT"
+(defproject clojusc/reggae "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
