27255e45a20fac384cb1743c9b9e7f7272c039d8
Duncan McGreggor <oubiwann@gmail.com>
2016-06-03 14:14:36 -0500

    Added requires.

--- dev-resources/src/reggae/dev.clj
+++ dev-resources/src/reggae/dev.clj
@@ -2,7 +2,11 @@
   (:require [clojure.tools.logging :as log]
             [clojure.tools.namespace.repl :as repl]
             [clojusc.twig :as logger]
-            [reggae.core :as reggae]))
+            [reggae.core :as reggae]
+            [reggae.interval :as interval]
+            [reggae.query :as query]
+            [reggae.rasj.types.interval :as rinterval]
+            [reggae.rasj.types.point :as rpoint]))
 
 (logger/set-level! '[reggae] :debug)
 
