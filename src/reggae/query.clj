298c3c9ed9b6937690acc34aea8ad9303302753a
Duncan McGreggor <oubiwann@gmail.com>
2016-06-29 09:36:13 -0500

    Cleaned up query functions

--- src/reggae/query.clj
+++ src/reggae/query.clj
@@ -1,15 +1,23 @@
 (ns reggae.query
   (:require [clojure.tools.logging :as log]
             [reggae.rasj.rasbag :as rasbag])
-  (:import [rasj.odmg RasBag]
-           [org.odmg DBag])
-  ;(:refer-clojure :exclude [hash])
-  )
+  (:import [org.odmg DBag]
+           [rasj RasStructure]
+           [rasj.odmg RasBag]))
+
+(defn get-elements
+  ""
+  [col]
+  (log/debug "Got result type " (type col))
+  (cond
+    (rasbag/rasbag? col)
+      (-> col
+          (rasbag/get-iterator)
+          (iterator-seq))))
 
 (defn run [client-obj query-str]
   (log/debugf "Running query '%s'..." query-str)
   (-> (.newOQLQuery client-obj)
       (#(do (.create % query-str) %))
       (.execute)
-      (rasbag/get-iterator)
-      (iterator-seq)))
+      (get-elements)))
