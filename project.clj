f2cec4a468170bc4c438bb5bc7491e819565ef54
Bjorn Wickstrom <bjorstrom@users.noreply.github.com>
2016-10-06 10:35:17 -0500

    Added test and uber profiles.
    Removed lint deps from dev profile (they're in the test profile).

--- project.clj
+++ project.clj
@@ -12,10 +12,17 @@
                  [org.clojure/tools.namespace "0.2.11"]
                  [clojusc/twig "0.2.3"]]
   :profiles {
+    :uber {
+      :aot :all}
+    :test {
+      :plugins [
+        [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
+        [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]]
+      :test-selectors {
+      :default :unit
+      :unit :unit
+      :system :system
+      :integration :integration}}
     :dev {
       :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                     [slamhound "1.5.5"]]
-      :plugins [[lein-kibit "0.1.2"]
-                [jonase/eastwood "0.2.3"]]
-      :aliases {"slamhound" ["run" "-m" "slam.hound"]}
       :source-paths ["dev-resources/src"]}})
