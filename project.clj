6dc1a7bc3e67ea2f9ff4598876a283fc89cf3082
Duncan McGreggor <duncan.mcgreggor@nasa.gov>
2017-09-25 08:37:44 -0500

    Added aliases.

--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repl-options {:init-ns reggae.dev}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [odmg/odmg "3.0"]
-                 [org.rasdaman/rasj "9.0.4"]
-                 [dire "0.5.4"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [clojusc/twig "0.2.6"]]
+  :dependencies [
+    [clojusc/twig "0.2.6"]]
+    [dire "0.5.4"]
+    [odmg/odmg "3.0"]
+    [org.clojure/clojure "1.8.0"]
+    [org.clojure/tools.namespace "0.2.11"]
+    [org.rasdaman/rasj "9.0.4"]
   :profiles {
     :uber {
       :aot :all}
@@ -24,4 +25,23 @@
         :integration :integration}}
     :dev {
       :dependencies [[org.clojure/tools.namespace "0.2.11"]]
-      :source-paths ["dev-resources/src"]}})
+      :source-paths ["dev-resources/src"]}}
+  :aliases {
+    "check-deps" [
+      "with-profile" "+test" "ancient" "check" ":all"]
+    "kibit" [
+      "with-profile" "+test" "do"
+        ["shell" "echo" "== Kibit =="]
+        ["kibit"]]
+    "outlaw" [
+      "with-profile" "+test"
+      "eastwood" "{:namespaces [:source-paths] :source-paths [\"src\"]}"]
+    "lint" [
+      "with-profile" "+test" "do"
+        ["check"] ["kibit"] ["outlaw"]]
+    "build" ["with-profile" "+test" "do"
+      ["check-deps"]
+      ["lint"]
+      ["test"]
+      ["compile"]
+      ["uberjar"]]})
